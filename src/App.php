2c9d973ffeffbfd19291e55542fa56f3d2e4871f
oromadin <osromadin@gmail.com>
2024-09-13 17:29:29 +0000

    add table, made object

--- src/App.php
+++ src/App.php
@@ -5,6 +5,15 @@ namespace App;
 class App {
 
   public static function main(){
-    echo 2;
+    $inputFileType = 'Xlsx';
+    $inputFileName = __DIR__ . '/../resources/Table.xlsx';
+    
+    $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($inputFileName);
+    //var_dump($spreadsheet);
+
+    echo '<pre>';
+    var_dump(get_class_methods($spreadsheet));
+    echo '</pre>';
+    //$dataArray = $spreadsheet->toArray();
   }
 }
