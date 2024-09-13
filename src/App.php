da274c34ada6aabc16373242fc5d4d02be82df6c
oromadin <osromadin@gmail.com>
2024-09-13 22:56:11 +0000

    change parameters

--- src/App.php
+++ src/App.php
@@ -7,21 +7,20 @@ use PhpOffice\PhpSpreadsheet\IOFactory;
 class App {
 
   public static function main() {
-    $table = self::readSpreadSheet();
+    $inputFileType = 'Xlsx';
+    $inputFileName = __DIR__ . '/../resources/Table.xlsx';
+    
+    $table = self::readSpreadSheet($inputFileType,$inputFileName);
     self::renderSpreadSheet($table);
   }
 
-  private static function readSpreadSheet() {
-    $inputFileType = 'Xlsx';
-    $inputFileName = __DIR__ . '/../resources/Table.xlsx';
-
+  private static function readSpreadSheet($inputFileType, $inputFileName) {
     $reader = IOFactory::createReader($inputFileType);
     $spreadsheet = $reader->load($inputFileName);
     return $spreadsheet->getActiveSheet()->toArray();
   }
 
   private static function  renderSpreadSheet($table) {
-    
     foreach ($table as $tableRow) { ?>
       <table>
         <tr>
